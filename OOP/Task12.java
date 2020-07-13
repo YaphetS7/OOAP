710e5abb1bff7d5258c1b8e4ce41438e2d4d9e82
YaphetS7 <interni17121999@mail.ru>
2020-07-14 00:16:04 +0300

    type inheritance

---
+++ OOP/Task12.java
@@ -0,0 +1,29 @@
+public class Student{
+    protected FormOfStudy formOfStudy;
+    protected Institute institute;
+
+}
+
+public class FormOfStudy{
+
+}
+
+public class FullTimeForm extends FormOfStudy{
+
+}
+
+public class Correspondence extends FormOfStudy{
+
+}
+
+public class Institute{
+
+}
+
+public class TechInstitute extends Institute{
+
+}
+
+public class PhilosophyInstitute extends Institute{
+
+}
